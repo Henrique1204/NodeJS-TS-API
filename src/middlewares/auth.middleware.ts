2453895f8d350a65e08fddc87aa35e09e1908b3b
unknown <pauloh16.pdr@gmail.com>
2021-04-22 23:38:56 -0300

    Implementar decodificacao do token

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,12 +1,24 @@
+import usuarioModel from "@models/usuario.model";
 import { NextFunction, Request, Response } from "express";
+import jwt from 'jsonwebtoken';
+import { UsuarioInterface } from "src/interfaces/usuario.interface";
 
 class AuthMiddleware {
-    public autorizarUsuarioPorToken(req: Request, res: Response, next: NextFunction) {
-        const token = req.query.token || req.headers['x-access-token'];
+    public async autorizarUsuarioPorToken(req: Request, res: Response, next: NextFunction) {
+        try {
+            const token = req.query.token || req.headers['x-access-token'];
 
-        if (!token) return res.status(401).send({ mensagem: 'Acesso restrito' });
-
-        return next();
+            if (!token) return res.status(401).send({ mensagem: 'Acesso restrito' });
+    
+            const usuarioToken = jwt.verify(token.toString(), 'SECRET') as UsuarioInterface;
+            const usuario = await usuarioModel.findById(usuarioToken._id);
+    
+            if (!usuario) return res.status(400).send({ mensagem: 'Usuário não existe' });
+    
+            return next();
+        } catch(e) {
+            return res.status(401).send({ mensagem: 'Token inválido!' });
+        }
     }
 }
 
